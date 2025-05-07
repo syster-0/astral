f4a9ee33e8b1fcf7d3eeea8bd0ce7143c4ce989b
会做饭的二哈 <baikaiwen12@outlook.com>
2025-05-07 14:15:51 +0800

    style: 修正导入路径的大小写问题
    将 `Aps.dart` 的导入路径更改为 `aps.dart`，以保持代码库中的命名一致性

--- lib/wid/windows_controls.dart
+++ lib/wid/windows_controls.dart
@@ -1,4 +1,4 @@
-import 'package:astral/k/app_s/Aps.dart';
+import 'package:astral/k/app_s/aps.dart';
 import 'package:flutter/material.dart';
 import 'package:system_tray/system_tray.dart';
 import 'dart:io';
