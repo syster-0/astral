a37eafbd99550ae053b8371e3803daaec234ec52
会做饭的二哈 <baikaiwen12@outlook.com>
2025-05-20 09:02:27 +0800

    refactor(vpn_service): 调整 DNS 服务器添加顺序并注释默认路由配置
    将 Google DNS 和 114 DNS 的添加顺序进行调整，并注释掉默认的路由配置，以便更清晰地管理 VPN 流量规则。

--- vpn_service_plugin/android/src/main/kotlin/com/plugin/vpn_service_plugin/TauriVpnService.kt
+++ vpn_service_plugin/android/src/main/kotlin/com/plugin/vpn_service_plugin/TauriVpnService.kt
@@ -124,11 +124,12 @@ class TauriVpnService : VpnService() {
         // builder.addDnsServer(dns)
 
         // 添加公共 DNS 服务器（如 Google DNS 或 114 DNS）
-        builder.addDnsServer("8.8.8.8");  // Google DNS 
         builder.addDnsServer("114.114.114.114");  // 114 DNS 
-        // 配置路由规则，确保所有流量通过 VPN 
-        builder.addRoute("0.0.0.0",  0); // IPv4 默认路由 
-        builder.addRoute("::0", 0); // IPv6 默认路由 
+        builder.addDnsServer("8.8.8.8");  // Google DNS 
+
+        // // 配置路由规则，确保所有流量通过 VPN 
+        // builder.addRoute("0.0.0.0",  0); // IPv4 默认路由 
+        // builder.addRoute("::0", 0); // IPv6 默认路由 
 
         // 添加路由规则
         for (route in routes) {
